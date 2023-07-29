1a9c283515420b37bf9f34c6d1df01da691d7186
Haliyah Tijani <haliyya460@gmail.com>
2023-07-29 14:14:08 +0100

    django setup

---
+++ main/api/views.py
@@ -0,0 +1,9 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        "api/token"
+    ]
+    return Response(routes)
