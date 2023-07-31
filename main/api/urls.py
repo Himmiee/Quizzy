eb306ec50113605c602c2b905e1ad23a0e9d6f45
Haliyah Tijani <haliyya460@gmail.com>
2023-07-31 21:13:39 +0100

    api setup

--- main/api/urls.py
+++ main/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from .views import CreateDetailView, RetrieveDetailView, ListDetailView, DetailView, CreateOptionView, CreateCourseView
 
 
 urlpatterns = [
-    path("", views.getRoutes, name="test"),
+    path("create/",CreateDetailView.as_view(), name="create_detail"),
+    path("get/",ListDetailView.as_view(), name="get"),
+    path("retrieve/<int:id>",RetrieveDetailView.as_view(), name="retrieve_detail"),
+    path("info/<int:id>",DetailView.as_view(), name="info"),
+    path("createOpt/",CreateOptionView.as_view(), name="create_option"),
+    path("createCourse/",CreateCourseView.as_view(), name="create_course"),
     
 ]
